99ccd9a7b48c9a4c52df5bdb363e081ca674cad2
cbk0315 <cbk0315@midasit.com>
2021-09-14 17:52:41 +0900

    refactor: Basic Vert.x-Web concepts

--- src/main/java/server/MainVerticle.java
+++ src/main/java/server/MainVerticle.java
@@ -3,19 +3,23 @@ package server;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
         HttpServer server = vertx.createHttpServer();
-        server.requestHandler(request -> {
-            HttpServerResponse response = request.response();
-            response.putHeader("content-type", "text/plain");
 
-            response.end("Hello World!");
+        Router router = Router.router(vertx);
+
+        router.route().handler(ctx -> {
+            HttpServerResponse response = ctx.response();
+            response.putHeader("content-type", "text/plain");
+            response.end("Hello World from Vert.x-Web!");
         });
 
-        server.listen(8080);
+        server.requestHandler(router)
+              .listen(8080);
     }
 }
